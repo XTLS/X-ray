v2fly/v2ray-core

Fix Dial function crash instance when there is no instance context in the ctx

functions.go
@@ -51,6 +51,11 @@ func Dial(ctx context.Context, v *Instance, dest net.Destination) (net.Conn, err
 	if dispatcher == nil {
 		return nil, newError("routing.Dispatcher is not registered in V2Ray core")
 	}
+
+	if ctx.Value(v2rayKey) == nil {
+		ctx = context.WithValue(ctx, v2rayKey, v)
+	}
+
 	r, err := dispatcher.(routing.Dispatcher).Dispatch(ctx, dest)
 	if err != nil {
 		return nil, err

XTLS/X-ray

core/functions.go
@@ -50,6 +50,11 @@ func Dial(ctx context.Context, v *Instance, dest net.Destination) (net.Conn, err
 	if dispatcher == nil {
 		return nil, newError("routing.Dispatcher is not registered in Xray core")
 	}
+
+	if ctx.Value(xrayKey) == nil {
+		ctx = context.WithValue(ctx, xrayKey, v)
+	}
+
 	r, err := dispatcher.(routing.Dispatcher).Dispatch(ctx, dest)
 	if err != nil {
 		return nil, err